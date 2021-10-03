ff7f11a2ae1d87f190b834c565280abaa7965b64
Riya Mathur <51375575+Blitzcoder01@users.noreply.github.com>
2021-10-03 20:48:53 +0530

    Create LargestSubarrayWith0Sum.java

---
+++ LargestSubarrayWith0Sum.java
@@ -0,0 +1,57 @@
+// { Driver Code Starts
+import java.util.*;
+
+class MaxLenZeroSumSub
+{
+
+    // Returns length of the maximum length subarray with 0 sum
+
+    // Drive method
+    public static void main(String arg[])
+    {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T > 0)
+        {
+            int n = sc.nextInt();
+            int arr[] = new int[n];
+            for (int i = 0; i < n; i++)
+                arr[i] = sc.nextInt();
+
+            GfG g = new GfG();
+            System.out.println(g.maxLen(arr, n));
+            T--;
+        }
+    }
+}// } Driver Code Ends
+
+
+class GfG
+{
+    int maxLen(int arr[], int n)
+    {
+        // Your code here
+        int ans=0;
+        int sum= 0;
+        int max= 0;
+        Map<Integer, Integer> map= new HashMap<>();
+        for(int i=0; i<n; i++){
+           sum= sum+arr[i];
+           if( sum==0){
+               max=i+1;
+           }
+           else if(map.containsKey(sum)){
+               ans= i-map.get(sum);
+               if(ans> max){
+                   max=ans;
+               }
+               
+           }
+           else{
+               map.put(sum, i);
+           }
+           
+        }
+        return max;
+    }
+}
